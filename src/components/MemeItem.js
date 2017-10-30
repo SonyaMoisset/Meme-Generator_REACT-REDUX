fffd0bd0bc76e5d5b986b04ea279066effa3cda5
smoisset <smoisset@worldremit.com>
2017-10-30 09:06:56 +0000

    Animate Meme Item Component

--- src/components/MemeItem.js
+++ src/components/MemeItem.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
 
 class MemeItem extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            hovered: false
+        }
+    }
+
     render() {
         return (
-            <div className="meme-item">
+            <div
+                className="meme-item"
+                onMouseEnter={() => this.setState({ hovered: true })}
+                onMouseLeave={() => this.setState({ hovered: false })}>
                 <img
                     src={this.props.meme.url}
                     alt={this.props.meme.name}
-                    className="meme-image" />
-                <p className="meme-text">{this.props.meme.name}</p>
+                    className={ this.state.hovered ? "meme-image darken-image" : "meme-image" } />
+                <p
+                    className={ this.state.hovered ? "meme-text" : "no-text" }>{this.props.meme.name}</p>
             </div>
         );
     }
