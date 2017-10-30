870252a1a74a95d179f3e3484987854068e7eb92
smoisset <smoisset@worldremit.com>
2017-10-30 09:00:57 +0000

    Create MemeItem Component

---
+++ src/components/MemeItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class MemeItem extends Component {
+    render() {
+        return (
+            <div className="meme-item">
+                <img
+                    src={this.props.meme.url}
+                    alt={this.props.meme.name}
+                    className="meme-image" />
+                <p className="meme-text">{this.props.meme.name}</p>
+            </div>
+        );
+    }
+}
+
+export default MemeItem
